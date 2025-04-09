de4e91f94e16b5e50fb2e63d1aa7fa63778910c2
Diego Vargas <vargas.santos.diegoalan@gmail.com>
2025-04-09 11:34:49 -0600

    Add files via upload
    No se si funcione, de hecho no se porque estoy estudiando esta carrera

---
+++ app/models/cart.php
@@ -0,0 +1,71 @@
+<?php
+    declare(strict_types=1);
+    require_once __DIR__ . '/../helpers/connection.php';
+    use Firebase\JWT\JWT;
+    
+    require_once $_SERVER["DOCUMENT_ROOT"] . '/comercio-backend-main/vendor/autoload.php';
+
+class cart{
+    private int $id_carrito;
+    private int $id_usuario;
+    private int $id_prod;
+    private int $cantidad;
+    private float $precio_unitario;
+
+    public function __construct(object $data,) {
+        $this->id_carrito = $data["id_carrito"];
+        $this->id_usuario = $data["id_usuario"];
+        $this->id_prod = $data["id_prod"];
+        $this->cantidad = $data["cantidad"];
+        $this->precio_unitario = $data["precio_unitario"];
+      }
+    
+    public static function get_products($product_id){
+        $query="SELECT ID_CARRITO, ID_USUARIO, ID_PROD, CANTIDAD, PRECIO_UNITARIO FROM CARRITO_COMPRAS WHERE id_prod= ?";
+        $params = array(
+            array(
+                "param_type" => "i",
+                "param_value" => $product_id
+            )
+        );
+        
+        $cartResult = $this->getDBResult($query, $params);
+        return $cartResult;
+    }
+
+    public static function updateCart($cantidad, $id_carrito, $id_prod){
+        $query = "UPDATE carrito_compras SET  cantidad = ? WHERE id_carrito= ? AND id_prod= ?";
+        
+        $params = array(
+            array(
+                "param_type" => "i",
+                "param_value" => $cantidad
+            ),
+            array(
+                "param_type" => "i",
+                "param_value" => $id_carrito
+            ),
+            array(
+                "param_type" => "i",
+                "param_value" => $id_prod
+            )
+        );
+        
+        $this->updateDB($query, $params);
+    }
+
+    function deleteCartItem($id_carrito)
+    {
+        $query = "DELETE FROM carrito_compras WHERE id_carrito = ?";
+        
+        $params = array(
+            array(
+                "param_type" => "i",
+                "param_value" => $id_carrito
+            )
+        );
+        
+        $this->updateDB($query, $params);
+    }
+
+}
