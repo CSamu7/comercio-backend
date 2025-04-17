dbc9d4780822d99f62f073118dc1ef23e8bb9a04
Jos_03 <jos333-@hotmail.com>
2025-04-16 21:05:08 -0600

    Correción de imagenes de productos, reseñas y contreseñas encriptadas

--- IS_Conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbHost = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "infiniteskyvf";
-$db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
-if ($db->connect_error) {
-    die("Error al conectarse con la BD: " . $db->connect_error);
-}
