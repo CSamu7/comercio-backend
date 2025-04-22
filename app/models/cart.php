940fa5c9240687a93cfa6b4980ccbd7ae5c9a516
CSamu7 <samuel.pdg@hotmail.com>
2025-04-21 20:45:08 -0600

    Modificada la función getShoppingCart

--- app/models/cart.php
+++ app/models/cart.php
@@ -1,36 +1,31 @@
 <?php
-    declare(strict_types=1);
-    require_once __DIR__ . '/../helpers/connection.php';
-    use Firebase\JWT\JWT;
+declare(strict_types=1);
+require_once __DIR__ . '/../helpers/connection.php';
+use Firebase\JWT\JWT;
     
-    require_once $_SERVER["DOCUMENT_ROOT"] . '/comercio-backend-main/vendor/autoload.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . '/comercio-backend-main/vendor/autoload.php';
 
-class cart{
-    private int $id_carrito;
+class Cart{
     private int $id_usuario;
-    private int $id_prod;
-    private int $cantidad;
-    private float $precio_unitario;
 
-    public function __construct(object $data,) {
-        $this->id_carrito = $data["id_carrito"];
-        $this->id_usuario = $data["id_usuario"];
-        $this->id_prod = $data["id_prod"];
-        $this->cantidad = $data["cantidad"];
-        $this->precio_unitario = $data["precio_unitario"];
-      }
+    public function __construct(int $id_usuario) {
+        $this->id_usuario = $id_usuario;
+    }
     
-    public static function get_products($product_id){
-        $query="SELECT ID_CARRITO, ID_USUARIO, ID_PROD, CANTIDAD, PRECIO_UNITARIO FROM CARRITO_COMPRAS WHERE id_prod= ?";
-        $params = array(
-            array(
-                "param_type" => "i",
-                "param_value" => $product_id
-            )
-        );
-        
-        $cartResult = $this->getDBResult($query, $params);
-        return $cartResult;
+    public function getShoppingCart(){
+        $db = new Database();
+        $connection = $db->connect_to_db();
+        $query="SELECT id_carrito, id_usuario, id_prod, cantidad, precio_unitario FROM carrito_compras WHERE id_usuario = ?";
+
+        $stmt = $connection->prepare($query);
+        $stmt->execute([this->$id_usuario]);
+        $results = $stmt->get_result();
+
+        if($results->num_rows <= 0){
+          Flight::jsonHalt(['msg'=>"No existe este usuario"], 400);
+        }
+
+        return $results;
     }
 
     public static function updateCart($cantidad, $id_carrito, $id_prod){
